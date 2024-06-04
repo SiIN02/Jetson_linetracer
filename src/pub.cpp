ce352133238212eb635abe2a6656c0c1e4525fbf
SiIN02 <119506516+SiIN02@users.noreply.github.com>
2024-06-04 15:52:59 +0900

    Update pub.cpp

--- src/pub.cpp
+++ src/pub.cpp
@@ -10,19 +10,19 @@
 using namespace cv;
 using namespace std;
 
-// std::string src = "nvarguscamerasrc sensor-id=0 ! \
-// 	video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
-//     format=(string)NV12 ! nvvidconv flip-method=0 ! video/x-raw, \
-//     width=(int)640, height=(int)360, format=(string)BGRx ! \
-// 	videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+std::string src = "nvarguscamerasrc sensor-id=0 ! \
+ 	video/x-raw(memory:NVMM), width=(int)640, height=(int)360, \
+     format=(string)NV12 ! nvvidconv flip-method=0 ! video/x-raw, \
+     width=(int)640, height=(int)360, format=(string)BGRx ! \
+ 	videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 
 int main(int argc, char * argv[])
 {
-    cv::VideoCapture cap("line7.mp4");  //영상 파일 불러오기
+    //cv::VideoCapture cap("line7.mp4");  //영상 파일 불러오기 영상으로 테스트할 경우
 
     rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("node_dxlpub");
-    auto cam_node = std::make_shared<rclcpp::Node>("cam_pub");  //cam노드
+    auto cam_node = std::make_shared<rclcpp::Node>("cam_pub");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
     auto mypub = node->create_publisher<geometry_msgs::msg::Vector3>("/topic_dxlpub", qos_profile );
     auto cam_pub = cam_node->create_publisher<sensor_msgs::msg::CompressedImage>("image/compressed", qos_profile ); //cam topic
@@ -31,16 +31,16 @@ int main(int argc, char * argv[])
     vel.y = 10;
     vel.z = 0;
 
-    rclcpp::WallRate loop_rate(30.0); //20hz->50msec
-
+    rclcpp::WallRate loop_rate(30.0);
+    
     std_msgs::msg::Header hdr;
     sensor_msgs::msg::CompressedImage::SharedPtr msg;
-    // cv::VideoCapture cap(src, cv::CAP_GSTREAMER);
-    // if (!cap.isOpened()) {
-    //     RCLCPP_ERROR(node->get_logger(), "Could not open video!");
-    //     rclcpp::shutdown();
-    //     return -1;
-    // }    //입력영상 에러처리
+    cv::VideoCapture cap(src, cv::CAP_GSTREAMER);
+    if (!cap.isOpened()) {
+        RCLCPP_ERROR(node->get_logger(), "Could not open video!");
+        rclcpp::shutdown();
+        return -1;
+    }    //입력영상 에러처리
     cv::Mat frame;
 
     //영상처리 복사
